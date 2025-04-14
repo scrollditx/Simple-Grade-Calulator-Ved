ceca00c5370622b830fc79208ba8f1619daa9c21
scrollditx <parekhveddharmeshbhai@gmail.com>
2025-04-14 10:52:36 +0530

    done

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -6,54 +6,55 @@ int main()
     int score;
     char grade;
 
-    cout << " Welcome to the Grade Calculator! \n";
-    cout << "-------------------------------------\n";
+    cout << " Welcome to the Grade Calculator!"<< endl;
+    cout << "-------------------------------------"<< endl;
     cout << " Please enter your score (0 - 100): ";
     cin >> score;
 
     if (score < 0 || score > 100)
     {
-        cout << " Invalid score! Please enter a number between 0 and 100.\n";
-        return 1;
+        cout << " Invalid score! Please enter a number between 0 and 100." << endl;
+        return 0;
     }
 
-    grade = (score >= 90) ? 'A' :
-            (score >= 80) ? 'B' :
-            (score >= 70) ? 'C' :
-            (score >= 40) ? 'D' : 'F';
+    grade = (score >= 90)   ? 'A'
+            : (score >= 80) ? 'B'
+            : (score >= 70) ? 'C'
+            : (score >= 40) ? 'D'
+                            : 'F';
 
-    cout << "\n Your Grade is: " << grade << "\n";
+    cout << " Your Grade is: " << grade << endl;
 
     switch (grade)
     {
-        case 'A':
-            cout << " Excellent Work! Keep shining! \n";
-            break;
-        case 'B':
-            cout << " Well done! You're doing great!\n";
-            break;
-        case 'C':
-            cout << " Good Job! Keep improving!\n";
-            break;
-        case 'D':
-            cout << " You passed, but there's room for improvement.\n";
-            break;
-        default:
-            cout << " Sorry, you failed. Don't give up!\n";
-            break;
+    case 'A':
+        cout << " Excellent Work! " << endl;
+        break;
+    case 'B':
+        cout << " Well done!" << endl;
+        break;
+    case 'C':
+        cout << " Good Job!" << endl;
+        break;
+    case 'D':
+        cout << " You passed, but you could do better." << endl;
+        break;
+    default:
+        cout << " Sorry, you failed." << endl;
+        break;
     }
 
     if (grade == 'A' || grade == 'B' || grade == 'C' || grade == 'D')
     {
-        cout << " Congratulations! You're eligible for the next level! \n";
+        cout << " Congratulations! You're eligible for the next level! " << endl;
     }
     else
     {
-        cout << " Keep working hard and try again next time! \n";
+        cout << " Keep working hard and try again next time! " << endl;
     }
 
-    cout << "-------------------------------------\n";
-    cout << "Thanks for using the Grade Calculator. \n";
+    cout << "-------------------------------------" << endl;
+    cout << "Thanks for using the Grade Calculator. " << endl;
 
     return 0;
 }
